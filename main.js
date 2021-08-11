03046c20e85589c68778122980979a06fd628105
Amit Kharka <amitkharka27@gmail.com>
2021-08-11 18:07:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const fs = require('fs');
+const cors = require('cors');
 const app = express();
 const port = 8090;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   var file = fs.createReadStream('./sample.pdf');
