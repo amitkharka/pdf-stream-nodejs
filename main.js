19213b4809308bd052ba7692244b38e939328e98
Amit Kharka <amitkharka27@gmail.com>
2021-08-11 18:06:10 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 8090;
+
+app.get('/', (req, res) => {
+  var file = fs.createReadStream('./sample.pdf');
+  var stat = fs.statSync('./sample.pdf');
+  res.setHeader('Content-Length', stat.size);
+  res.setHeader('Content-Type', 'application/pdf');
+  res.setHeader('Content-Disposition', 'attachment; filename=quote.pdf');
+  file.pipe(res);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
